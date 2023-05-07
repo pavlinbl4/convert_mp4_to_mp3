37f9b93fcba7c667e8e37a9aa7991869bf500774
evgeniy_air <pavlinbl4@gmail.com>
2023-05-07 23:26:20 +0300

    refactoring

--- converter/main_modul.py
+++ converter/main_modul.py
@@ -1,6 +1,5 @@
 from filename_or_extension import select_file_name
 from files_work import find_files
-from gui_window.window import gui_window
 from mp3_converter import convert_mp4_to_mp3
 from tkinter import filedialog
 
@@ -14,8 +13,3 @@ def work_with_file(folders_paths, extension, final_extension):
     for file in files_list:
         convert_mp4_to_mp3(f'{folders_paths[1]}/{file}',
                            f'{folders_paths[2]}/{select_file_name(file)}.{final_extension}')
-
-
-if __name__ == '__main__':
-    folders_paths = gui_window()
-    work_with_file(folders_paths, 'mp4', 'mp3')
