0ac45f89f07eeb6b69950123dceeda9bf1bef92d
evgeniy_air <pavlinbl4@gmail.com>
2023-05-07 23:16:11 +0300

    changed to folders_paths

--- main_modul.py
+++ main_modul.py
@@ -1,13 +1,13 @@
-from filename_or_extension import select_file_name
-from files_work import find_files
-from mp3_converter import convert_mp4_to_mp3
+from converter.filename_or_extension import select_file_name
+from converter.files_work import find_files
+from converter.mp3_converter import convert_mp4_to_mp3
 
 
-def work_with_file(dir_path, extension, final_extension):
-    files_list = find_files(dir_path, extension)  # files list
+def work_with_file(folders_paths, extension, final_extension):
+    files_list = find_files(folders_paths[1], extension)  # files list
     for file in files_list:
-        convert_mp4_to_mp3(f'{dir_path}/{file}', f'{dir_path}/{select_file_name(file)}.{final_extension}')
+        convert_mp4_to_mp3(f'{folders_paths[1]}/{file}',
+                           f'{folders_paths[2]}/{select_file_name(file)}.{final_extension}')
 
-
-if __name__ == '__main__':
-    work_with_file('/Volumes/big4photo/Movies/shum', 'mp4', 'mp3')
+# if __name__ == '__main__':
+#     work_with_file('/Volumes/big4photo/Movies/shum', 'mp4', 'mp3')
